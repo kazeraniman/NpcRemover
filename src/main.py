a2a37be5dc7c0829263cb0431872028c0b006849
Iman Kazerani <kazeraniman@gmail.com>
2025-01-08 23:55:14 -0500

    Fixing the script so that it can be called from the batch file. Updating the batch file to get rid of the unnecessary quotation marks.

--- src/main.py
+++ src/main.py
@@ -3,13 +3,12 @@ import sys
 
 from pathlib import Path
 
-from src.gui import Gui
+from gui import Gui
 
-RES_FOLDER_PATH = Path('../res/')
+RES_FOLDER_PATH = Path(f'{Path(__file__).parent.parent}/res/')
 ID_JSON_FILE_PATH = Path.joinpath(RES_FOLDER_PATH, 'ids.json')
 
 if __name__ == '__main__':
-
     # Read in the IDs
     RES_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
     if ID_JSON_FILE_PATH.exists():
