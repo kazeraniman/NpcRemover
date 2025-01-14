d7ccc3ca043e3405a69b32598b5f8dde9d80ebee
Iman Kazerani <kazeraniman@gmail.com>
2025-01-13 20:26:43 -0500

    Adding in the buttons and the supporting functionality to copy the mod files, clear the mod folder, as well as clear the selected replacements.

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -9,13 +9,17 @@ from PyQt6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QLabel, QHBoxLayo
 from src.config import Config
 from src.gui.npc_list_item import NpcListItem
 from src.gui.npc_item import NpcItem
+from src.util import copy_files, CopyFileResult, clear_files
 
-MIN_WINDOW_WIDTH = 750
-MIN_WINDOW_HEIGHT = 500
+MIN_WINDOW_WIDTH = 800
+MIN_WINDOW_HEIGHT = 600
 
 WINDOW_TITLE_TEXT = 'NPC Remover'
 FOLDER_LABEL_TEXT = 'Mod Engine 2 Mod Folder'
 BROWSE_BUTTON_TEXT = 'Browse'
+COPY_BUTTON_TEXT = 'Replace NPCs'
+DELETE_BUTTON_TEXT = 'Clear Folder'
+CLEAR_BUTTON_TEXT = 'Clear Selection'
 REPLACED_NPC_LABEL_TEXT = 'Replaced NPCs'
 AVAILABLE_NPC_LABEL_TEXT = 'Available NPCs'
 
@@ -24,6 +28,18 @@ MOD_FOLDER_SELECT_TITLE_TEXT = 'Select Mod Engine 2 Mod Folder'
 MOD_FOLDER_REQUIRED_NAME = 'mod'
 MOD_FOLDER_REQUIRED_NAME_TITLE = 'Must Select Mod Folder'
 MOD_FOLDER_REQUIRED_NAME_BODY = 'The folder you chose is not called "mod". You must select the "mod" folder from Mod Engine 2.'
+REPLACE_NPC_NO_SELECTION_TITLE = 'No NPCs Selected'
+REPLACE_NPC_NO_SELECTION_BODY = 'Select some NPCs and then try again.'
+REPLACE_NPC_CONFIRMATION_TITLE = 'Replace NPCs'
+REPLACE_NPC_CONFIRMATION_BODY = 'Are you sure you wish to replace the selected NPCs?'
+REPLACE_NPC_SUCCESS_TITLE = 'NPCs Replaced'
+REPLACE_NPC_SUCCESS_BODY = 'The selected NPCs have been replaced.'
+REPLACE_NPC_ERROR_TITLE = 'Replacement Files Incorrect'
+REPLACE_NPC_ERROR_BODY = 'There should be one and only one of each file type in the res folder, and they should have matching names.\nPlease make sure you only have one of each of the following files with the same name in the res folder: anibnd.dcx, chrbnd.dcx, _h.texbnd.dcx, _l.texbnd.dcx.\nFor example, if you have a file called turtle.anibnd.dcx, the other files should be called turtle.chrbnd.dcx, turtle_h.texbnd.dcx, and turtle_l.texbnd.dcx. If you need help, try re-downloading the mod which comes with these files pre-set.'
+CLEAR_FOLDER_CONFIRMATION_TITLE = 'Clear Folder'
+CLEAR_FOLDER_CONFIRMATION_BODY = 'WARNING: This action will also remove mods which are not managed by this application. Are you sure you wish to clear the character mod folder?'
+CLEAR_FOLDER_SUCCESS_TITLE = 'Folder Cleared'
+CLEAR_FOLDER_SUCCESS_BODY = 'The character mod folder has been cleared.'
 
 LIST_KEY = 'list'
 
@@ -64,6 +80,10 @@ class MainWindow(QMainWindow):
         self.replaced_list_box.itemSelectionChanged.connect(self.replaced_list_selected)
         main_layout.addWidget(self.replaced_list_box)
 
+        clear_selection_button = QPushButton(CLEAR_BUTTON_TEXT)
+        clear_selection_button.clicked.connect(self.clear_button_clicked)
+        main_layout.addWidget(clear_selection_button)
+
         main_layout.addWidget(QLabel(AVAILABLE_NPC_LABEL_TEXT))
 
         self.search_box = QLineEdit()
@@ -79,6 +99,17 @@ class MainWindow(QMainWindow):
             self.add_row_item(self.available_list_box, npc)
 
         self.available_list_box.sortItems()
+
+        bottom_action_button_layout = QHBoxLayout()
+        main_layout.addLayout(bottom_action_button_layout)
+
+        delete_button = QPushButton(DELETE_BUTTON_TEXT)
+        delete_button.clicked.connect(self.delete_button_clicked)
+        bottom_action_button_layout.addWidget(delete_button)
+
+        copy_button = QPushButton(COPY_BUTTON_TEXT)
+        copy_button.clicked.connect(self.copy_button_clicked)
+        bottom_action_button_layout.addWidget(copy_button)
 
     @staticmethod
     def add_row_item(list_widget: QListWidget, npc: dict) -> NpcListItem:
@@ -146,6 +177,35 @@ class MainWindow(QMainWindow):
         message_box.setStandardButtons(buttons)
         message_box.setDefaultButton(default_button)
         return message_box.exec()
+
+    def copy_button_clicked(self):
+        ids_to_replace = [self.replaced_list_box.item(x).npc_item.id for x in range(self.replaced_list_box.count())]
+        if len(ids_to_replace) == 0:
+            self.show_message_box(REPLACE_NPC_NO_SELECTION_TITLE, REPLACE_NPC_NO_SELECTION_BODY)
+            return
+
+        should_replace = self.show_message_box(REPLACE_NPC_CONFIRMATION_TITLE, REPLACE_NPC_CONFIRMATION_BODY, QMessageBox.StandardButton.Cancel | QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.Yes)
+        if should_replace != QMessageBox.StandardButton.Yes:
+            return
+
+        copy_result = copy_files(ids_to_replace, self.mod_engine_folder_text.text())
+        if copy_result == CopyFileResult.SUCCESS:
+            self.show_message_box(REPLACE_NPC_SUCCESS_TITLE, REPLACE_NPC_SUCCESS_BODY)
+            return
+
+        if copy_result == CopyFileResult.ERROR:
+            self.show_message_box(REPLACE_NPC_ERROR_TITLE, REPLACE_NPC_ERROR_BODY)
+
+    def delete_button_clicked(self):
+        should_clear = self.show_message_box(CLEAR_FOLDER_CONFIRMATION_TITLE, CLEAR_FOLDER_CONFIRMATION_BODY, QMessageBox.StandardButton.Cancel | QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.Yes)
+        if should_clear != QMessageBox.StandardButton.Yes:
+            return
+
+        clear_files(self.mod_engine_folder_text.text())
+        self.show_message_box(CLEAR_FOLDER_SUCCESS_TITLE, CLEAR_FOLDER_SUCCESS_BODY)
+
+    def clear_button_clicked(self):
+        self.replaced_list_box.clear()
 
     @staticmethod
     def start(id_dict: dict, config: Config):
--- src/util.py
+++ src/util.py
@@ -1,11 +1,15 @@
 import json
+import shutil
 import urllib.parse
 import webbrowser
+from enum import Enum
 from pathlib import Path
+from typing import List
 
 BASE_FOLDER_PATH = Path(__file__).parent.parent
 RES_FOLDER_PATH = Path.joinpath(BASE_FOLDER_PATH, 'res/')
 ID_JSON_FILE_PATH = Path.joinpath(RES_FOLDER_PATH, 'ids.json')
+CHR_FOLDER_NAME = 'chr'
 
 FILE_READ_MODE = 'r'
 FILE_WRITE_MODE = 'w'
@@ -13,6 +17,14 @@ FILE_WRITE_MODE = 'w'
 WIKI_URL = 'https://eldenring.wiki.gg/wiki/Special:Search?{0}&go=Go&ns0=1'
 SEARCH_QUERY_PARAM = 'search'
 
+ANIBND_FILE_SUFFIX = '.anibnd.dcx'
+CHRBND_FILE_SUFFIX = '.chrbnd.dcx'
+HTEXBND_FILE_SUFFIX = '_h.texbnd.dcx'
+LTEXBND_FILE_SUFFIX = '_l.texbnd.dcx'
+
+class CopyFileResult(Enum):
+    SUCCESS = 0
+    ERROR = 1
 
 def read_id_file() -> dict:
     with open(ID_JSON_FILE_PATH, FILE_READ_MODE) as f:
@@ -20,3 +32,40 @@ def read_id_file() -> dict:
 
 def open_wiki(npc_name: str):
     webbrowser.open(WIKI_URL.format(urllib.parse.urlencode({SEARCH_QUERY_PARAM: npc_name})))
+
+def copy_files(id_list: List[str], mod_folder: str):
+    anibnd_files = list(RES_FOLDER_PATH.glob(f'*{ANIBND_FILE_SUFFIX}'))
+    if len(anibnd_files) != 1:
+        return CopyFileResult.ERROR
+
+    replaced_part = anibnd_files[0].name[:-sum(len(s) for s in anibnd_files[0].suffixes)]
+    chrbnd_files = list(RES_FOLDER_PATH.glob(f'{replaced_part}{CHRBND_FILE_SUFFIX}'))
+    htexbnd_files = list(RES_FOLDER_PATH.glob(f'{replaced_part}{HTEXBND_FILE_SUFFIX}'))
+    ltexbnd_files = list(RES_FOLDER_PATH.glob(f'{replaced_part}{LTEXBND_FILE_SUFFIX}'))
+
+    if not (len(chrbnd_files) == 1 and len(htexbnd_files) == 1 and len(ltexbnd_files) == 1):
+        return CopyFileResult.ERROR
+
+    mod_folder_path = Path(mod_folder)
+    chr_folder = mod_folder_path.joinpath(CHR_FOLDER_NAME)
+    chr_folder.mkdir(exist_ok=True)
+
+    for npc_id in id_list:
+        copy_file(anibnd_files[0], chr_folder, npc_id, replaced_part)
+        copy_file(chrbnd_files[0], chr_folder, npc_id, replaced_part)
+        copy_file(htexbnd_files[0], chr_folder, npc_id, replaced_part)
+        copy_file(ltexbnd_files[0], chr_folder, npc_id, replaced_part)
+
+    return CopyFileResult.SUCCESS
+
+def clear_files(mod_folder: str):
+    mod_folder_path = Path(mod_folder)
+    chr_folder = mod_folder_path.joinpath(CHR_FOLDER_NAME)
+    if not chr_folder.exists():
+        return
+
+    for file in chr_folder.glob("*"):
+        file.unlink()
+
+def copy_file(original_path: Path, chr_folder: Path, npc_id: str, existing_id: str):
+    shutil.copy(original_path, chr_folder.joinpath(original_path.name.replace(existing_id, npc_id)))
