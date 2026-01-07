d1b6b0a1a081014bde50b931db07913aed5bab29
Iman Kazerani <kazeraniman@gmail.com>
2026-01-07 11:36:05 -0500

    Fixing the error checking for the batch file.

--- launch_npc_remover.bat
+++ launch_npc_remover.bat
@@ -5,10 +5,11 @@ rem Setting up the container to hold all of the python dependencies needed to ru
 echo Setting up environment for the dependencies...
 if not exist venv (
     py -m venv venv
-    if not %errorlevel% == 0 (
+    if errorlevel 1 (
         python -m venv venv
-        if %errorlevel% == 0 (
-            echo Error: Unable to create environment.
+        if errorlevel 1 (
+            echo:
+            echo Error: Unable to create environment. Please check that you have Python3 installed and try again.
             pause
             exit \b 1
         )
@@ -20,16 +21,30 @@ if not exist venv (
 )
 
 rem Enter the environment and install any dependencies as needed.
+echo:
 echo Installing dependencies...
 venv\Scripts\pip install -r requirements.txt
+if errorlevel 1 (
+    echo:
+    echo Error: Unable to install dependencies.
+    pause
+    exit \b 1
+)
 echo Dependencies installed.
 
+echo:
 echo Launching the app...
 rem Change the working directory of the script so the different files can find each other.
 set PYTHONPATH=%~dp0
 
 rem Run the script to launch the app.
 venv\Scripts\python.exe src\main.py
+if errorlevel 1 (
+    echo:
+    echo Error: Unable to launch the app.
+    pause
+    exit \b 1
+)
 
 rem Pause to give time to read any error messages.
 pause
